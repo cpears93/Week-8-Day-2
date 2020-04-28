d72e048590c83cb5a8489bbaff69e766c03d17c3
Christopher Pearson <cpearson3140@gmail.com>
2020-04-28 16:05:34 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <div>
+    <input type="number" name="Amount" v-model.number="amount">
   </div>
+  <div>
+    <form v-if="amount">
+      <select v-on:change="handleSelectFrom"v-model="from">
+        <option disabledValue="">Select a Currency</option>
+        <option v-for="(currency, index) in currencies":value="currency"> {{currency.name}}
+        </option>
+      </select>
+    </form>
+    <form v-if="amount">
+      <select v-on:change="handleSelectTo"v-model="to">
+        <option disabledValue="">Select a Currency</option>
+        <option v-for="(currency, index) in currencies":value="currency"> {{currency.name}}
+        </option>
+      </select>
+    </form>
+    <div v-if="from&&to">
+      {{conversion}}
+    </div>
+  </div>
+
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data(){
+    return {
+      currencies: [],
+      from: null,
+      to: null,
+      amount: null,
+      conversion: 0
+  }},
+  mounted(){
+    fetch("https://api.exchangeratesapi.io/latest")
+    .then(result => result.json())//.then(data => this.currencies = data.rates
+    .then(data => {
+      for (let key of Object.keys(data.rates)) {
+        console.log(key);
+      const newCurrency = {name: key,
+                    value: data.rates[key]}
+      this.currencies.push(newCurrency)}
+    })
+  },
+  computed: {
+    converting: function(){
+      this.conversion = (this.from.value*this.amount)/this.to.value
+    }
+  },
+  watch: {
+    conversion: function(){
+      this.converting()
+    }
   }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
